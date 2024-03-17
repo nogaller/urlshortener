3dfff21170a5790ea8f2d7b4da0e14a7e721dc03
Mau, Oliver <oliver.mau@accenture.com>
2024-03-17 23:22:28 +0100

    initial commit

---
+++ src/main/java/io/twodigits/urlshortener/model/URL.java
@@ -0,0 +1,14 @@
+package io.twodigits.urlshortener.model;
+
+public class URL {
+    /**
+     * The unique ID of an URL
+     */
+    private String id;
+
+    /**
+     * The URL for which a short URL is provided
+     */
+    private String url;
+
+}
