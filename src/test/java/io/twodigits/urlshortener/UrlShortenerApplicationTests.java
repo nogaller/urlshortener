07149734864bfe1102cef18d14bfa816bd79b417
Anton <Anton@Anton-Lenovo.fritz.box>
2024-07-24 14:41:04 +0200

    init root controller

--- src/test/java/io/twodigits/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/io/twodigits/urlshortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,22 @@
 package io.twodigits.urlshortener;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UrlShortenerApplicationTest {
+class UrlShortenerApplicationTests {
+
+	@Autowired
+	private HomeController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+
+		assertThat(controller.greeting()).isEqualTo("API root");
 	}
 
 }
