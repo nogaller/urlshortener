3814938a78f20e6081e95e546ce318d6225f9bcd
Anton <Anton@Anton-Lenovo.fritz.box>
2024-08-02 17:15:47 +0200

    use SQLite DB

---
+++ src/main/java/io/twodigits/urlshortener/DataSourceBean.java
@@ -0,0 +1,27 @@
+package io.twodigits.urlshortener;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class DataSourceBean {
+
+	@Bean
+	@Primary
+	DataSourceProperties getDatasourceProperties() {
+		return new DataSourceProperties();
+	}
+
+	@Bean
+	DataSource dataSource() {
+		return getDatasourceProperties()//
+				.initializeDataSourceBuilder()//
+				.build();
+	}
+}
